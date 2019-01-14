c69bf806b7cafbd5df409b602ce2cb07db1d4878
andrew u frank <frank@santafe.local>
2019-01-14 22:15:46 +0100

    closer - a json record is produced, but ??

--- make-html.sh
+++ make-html.sh
@@ -4,9 +4,15 @@
 # changed for input in YAML-metadata
 # 	output html
 
-pandoc --filter pandoc-citeproc \
-     --bibliography="BibTexExample.bib" --standalone \
-     "$1.md" -o "$1.html" && \
-     cat "$1.html"
+pandoc --filter pandoc-citeproc      "$1.md" -o "$1.html" 
+#     --bibliography="BibTexExample.bib" \ # --standalone \
+
+echo $1.html
+FF=" {\"body\" : \""
+EE="\"}"
+PL=`cat $1.html`
+ 
+X="$FF$PL$EE"
+echo $X 
 
 
